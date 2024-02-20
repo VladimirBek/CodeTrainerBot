263f86cf80760cdba48b69b357d506777592d071
VladimirBek <vladimirbek1996@gmail.com>
2024-02-20 18:52:47 +0300

    tests added

--- tests/exercise_parser_tests.py
+++ tests/exercise_parser_tests.py
@@ -1,16 +1,30 @@
-import unittest
+from unittest.mock import MagicMock
+
+import pytest
 
 from app.src.exercise_parser import ExerciseParser
 
-events = []
+
+@pytest.mark.asyncio
+async def test_get_urls():
+    exercise_parser = ExerciseParser()
+    exercise_parser.session.get = MagicMock(return_value=MagicMock(status_code=200, html=MagicMock(find=lambda x: [MagicMock(text='10')])))
+
+    urls = await exercise_parser.get_urls()
+    assert len(urls) == 10
 
 
-class TestSParserMethods(unittest.IsolatedAsyncioTestCase):
+@pytest.mark.asyncio
+async def test_get_rows():
+    exercise_parser = ExerciseParser()
+    exercise_parser.get_task = MagicMock(return_value='Test task text')
 
-    async def asyncSetUp(self):
-        self._async_connection = ExerciseParser()
-        events.append(self._async_connection)
+    mock_db = MagicMock()
+    mock_db.execute = MagicMock()
 
-    async def test_get_exercises(self):
-        result = await self._async_connection.get_urls()
-        self.assertIsInstance(result, list[str])
+    exercise_parser.session.get = MagicMock(return_value=MagicMock(status_code=200, html=MagicMock(find=lambda x: [MagicMock(find=lambda x: ['Test statement'])])))
+
+    await exercise_parser.get_rows(MagicMock(), ['mock_url'])
+
+    assert exercise_parser.get_task.call_count == 1
+    assert mock_db.execute.call_count == 1
